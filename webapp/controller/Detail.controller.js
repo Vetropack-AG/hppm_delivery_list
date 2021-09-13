49e7ad56b6b209dc26f716c447e41c3df3ab3316
Herbert Kaintz <herbert.kaintz@concircle.com>
2021-09-13 14:27:57 +0000

    fixed item status display

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -491,12 +491,14 @@ sap.ui.define([
 									new sap.m.ObjectStatus({
 										text: "{i18n>details.quantityResult}",
 										icon: "{= ${QuantityResult} === true ? 'sap-icon://sys-enter-2' : 'sap-icon://message-error' }",
-										state: "{= ${QuantityResult} === true ? 'Success' : 'Error' }"
+										state: "{= ${QuantityResult} === true ? 'Success' : 'Error' }",
+										visible: "{= ${InspectionStatus} === 'QUANTITY' || ${InspectionStatus} === 'QUALITY' || ${InspectionStatus} === 'POSTED' }"
 									}),
 									new sap.m.ObjectStatus({
 										text: "{i18n>details.qualityResult}",
 										icon: "{= ${QualityResult} === true ? 'sap-icon://sys-enter-2' : 'sap-icon://message-error' }",
-										state: "{= ${QualityResult} === true ? 'Success' : 'Error' }"
+										state: "{= ${QualityResult} === true ? 'Success' : 'Error' }",
+										visible: "{= ${InspectionStatus} === 'QUALITY' || ${InspectionStatus} === 'POSTED' }"
 									})
 								]
 							}).addStyleClass("sapUiSmallMarginBegin sapUiSmallMarginEnd")
